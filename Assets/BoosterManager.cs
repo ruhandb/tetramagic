533e661fcdfc536a38766c9d5b2aa32c7d3c049e
Ruhan Borgmann <ruhandb@gmail.com>
2020-06-28 21:42:29 -0300

    Open Booster

---
+++ Assets/BoosterManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoosterManager : MonoBehaviour
+{
+
+    public GameObject TopBooster;
+
+    public GameObject Card1;
+    public GameObject Card2;
+    public GameObject Card3;
+
+    private AudioManager audioManager;
+
+    private void Awake()
+    {
+        audioManager = FindObjectOfType<AudioManager>();
+    }
+
+    public void OpenBooster()
+    {
+        var seq = LeanTween.sequence();
+        var pos = new Vector2(TopBooster.transform.position.x + 1000, TopBooster.transform.position.y + 100);
+        seq.append(LeanTween.move(TopBooster, pos, 2));
+        LeanTween.move(TopBooster, pos, 2);
+        audioManager.Play("RipPaper ");
+    }
+}
